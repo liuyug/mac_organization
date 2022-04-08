511885448a6f68e0bea8d3bd7e76766e1e4233ea
Yugang LIU <liuyug@gmail.com>
2022-04-09 06:32:46 +0800

    first commit

---
+++ mac.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+
+import json
+import csv
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def load_mac_file(oui_file):
+    mac_orgs = []
+    with open(oui_file) as f:
+        line_no = 1
+        group_no = 0
+        group_items = []
+        for line in f.readlines():
+            line_no += 1
+            if not line or line == '\n':
+                if group_no == 0:
+                    # header
+                    pass
+                else:
+                    items = group_items[0].split('\t')
+                    mac_hex = items[0].split(' ')[0]
+                    company = items[-1].strip()
+                    items = group_items[1].split('\t')
+                    mac_base16 = items[0].split(' ')[0]
+                    company2 = items[-1].strip()
+                    assert company == company2
+                    if len(group_items) > 2:
+                        address = group_items[2].strip()
+                        address2 = group_items[3].strip()
+                        country = group_items[4].strip()
+                    else:
+                        address = None
+                        address2 = None
+                        country = None
+                    mac_orgs.append({
+                        'hex': mac_hex,
+                        'base16': mac_base16,
+                        'company': company,
+                        # 'company2': company2,
+                        'address': address,
+                        'address2': address2,
+                        'country': country,
+                    })
+                    print(f'{group_no:03d} {line_no:03d} {mac_hex} {company}')
+                group_no += 1
+                group_items = []
+            else:
+                group_items.append(line)
+
+    return mac_orgs
+
+
+def output(mac_orgs, csv_file=None, json_file=None):
+    if json_file:
+        json.dump(mac_orgs, open(json_file, 'w'))
+        print(f'Output {json_file}')
+    if csv_file:
+        with open(csv_file, 'w', newline='') as f_csv:
+            header = ['hex', 'base16', 'company', 'address', 'address2', 'country']
+            writer = csv.DictWriter(f_csv, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(mac_orgs)
+        print(f'Output {csv_file}')
+
+
+def main():
+    name = 'app'
+    version = '1.0.0'
+    description = 'Application Description'
+    about = f'{name} v{version} {description}'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--version', action='version', version=about,
+                        help='show version')
+    parser.add_argument('-v', '--verbose', action='count',
+                        default=0, help='verbose output')
+
+    group = parser.add_argument_group('group')
+    group.add_argument('--input', help='input oui file')
+    group.add_argument('--json', help='output json')
+    group.add_argument('--csv', help='output csv')
+
+    args = parser.parse_args()
+    # log to stdout in cli mode
+    level = logging.INFO - args.verbose * 10
+    logging.basicConfig(
+        level=level,
+        format='%(message)s',
+    )
+
+    if args.input:
+        oui_file = args.input
+        mac_orgs = load_mac_file(oui_file)
+        if args.json:
+            output(mac_orgs, json_file=args.json)
+        if args.csv:
+            output(mac_orgs, csv_file=args.csv)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,23 @@
+
+#ieee mac address standard
+https://standards-oui.ieee.org/oui/oui.txt
+
+```
+F4-BD-9E   (hex)		Cisco Systems, Inc
+F4BD9E     (base 16)		Cisco Systems, Inc
+				80 West Tasman Drive
+				San Jose  CA  94568
+				US
+
+58-85-E9   (hex)		Realme Chongqing MobileTelecommunications Corp Ltd
+5885E9     (base 16)		Realme Chongqing MobileTelecommunications Corp Ltd
+				No.24 Nichang Boulevard, Huixing Block, Yubei District, Chongqing.
+				Chongqing  China  401120
+				CN
+
+BC-23-92   (hex)		BYD Precision Manufacture Company Ltd.
+BC2392     (base 16)		BYD Precision Manufacture Company Ltd.
+				No.3001, Bao He Road, Baolong Industrial, Longgang Street,Longgang Zone, Shenzhen
+				shenzhen    518116
+				CN
+```
